e1cf7a3814813f1d8bc596489902cc655f25408e
BrightWhisper <sureforwsy@163.com>
2019-09-17 23:18:33 +0800

    [feature]添加icon和icon-position属性

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,58 @@
 <template>
-    <button class="w-button">按钮</button>
+    <button class="w-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        props: ['icon', 'iconPosition']
+    }
 </script>
 <style lang="scss">
-    .w-button{
+    .w-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
+
+        &:active {
             border-color: var(--button-active-bg);
         }
-        &:focus{
-            outline:none;
+
+        &:focus {
+            outline: none;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+            margin-left: 0;
+        }
+        > .content{
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .3em;
+                margin-right: 0;
+            }
+            > .content{
+                order: 1;
+            }
         }
     }
 </style>
